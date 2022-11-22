92104c255eedd9f8d3ad6e796c354613363f9ec5
maks1394 <gusevmax9@yandex.com>
2022-11-22 13:49:44 +0300

    HW3

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -56,7 +56,7 @@ function HW2() {
 
     return (
         <div id={'hw2'}>
-            <div className={s2.hwTitle}>Homework #2</div>
+            <div className={s2.hwTitle}><p>Homework #2</p></div>
             <div className={s2.hw}>
                 <Affairs
                     data={filteredAffairs}
